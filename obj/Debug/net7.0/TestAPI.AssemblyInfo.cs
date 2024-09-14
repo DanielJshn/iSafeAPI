46d5c9a28c92891d6d6c1b80c6914e92831a0eec
DanielDobosh <danieldobosh362@gmail.com>
2024-09-14 15:20:18 +0300

    Temporary commit before merge

--- obj/Debug/net7.0/TestAPI.AssemblyInfo.cs
+++ obj/Debug/net7.0/TestAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af682f7a88aa83d6b439745f7dda8ddf5cc1025f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
